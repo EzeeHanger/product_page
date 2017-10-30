cb1d2708a1811b685a421b3a1fba734c45cbfd9e
01jorden01 <jorden.ysewyn@student.howest.be>
2017-10-30 15:11:07 +0100

    Carousel Pause/Play fix

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+function init() {
+    $(function () {
+        $('#myCarousel').carousel({
+            interval: 8000,
+            pause: 'false'
+        });
+        $('#playButton').click(function () {
+            $('#myCarousel').carousel('cycle');
+        });
+        $('#pauseButton').click(function () {
+            $('#myCarousel').carousel('pause');
+        });
+    });
+}
+init();
